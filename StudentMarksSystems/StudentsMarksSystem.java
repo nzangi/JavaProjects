355a0913d1d8d42ac6611ce28d304bbd1f23d99b
nzangi <nzangimuoki284@gmail.com>
2023-12-19 12:20:27 +0300

    Student ID and performance added

--- StudentMarksSystems/StudentsMarksSystem.java
+++ StudentMarksSystems/StudentsMarksSystem.java
@@ -5,22 +5,22 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 //main method
-//enter the student index and marks
+//enter the student name
+// Enter each student marks for 5 subjects
 //calculate the average
 //Give excellent, good, fair fail according to student average
 //give class average
 public class StudentsMarksSystem {
     public static void main(String[] args){
         ArrayList<String> studentsInformation = giveEachStudentMarks();
-        System.out.println("|---------------------------------------------------------------------------------------------------------------|");
-        System.out.println("|| ENGLISH  || KISWAHILI || MATHS  || SCIENCE || SOCIAL STUDIES || STUDENT TOTAL MARKS || STUDENT TOTAL MARKS  ||" );
-        System.out.println("|---------------------------------------------------------------------------------------------------------------|");
+        System.out.println("|---------------------------------------------------------------------------------------------------------------------------------------|");
+        System.out.println("| STDID || STD NAME || ENGLISH  || KISWAHILI || MATHS  || SCIENCE || S/ STUDIES || STD TOTAL MARKS || STD AVG MARKS  || STD PERFORMANCE | " );
+        System.out.println("|---------------------------------------------------------------------------------------------------------------------------------------|");
         for (String studentInformation : studentsInformation) {
             String result = studentInformation.replaceAll("[\\[\\],]", "");
             System.out.println(result);
         }
-        System.out.println("|---------------------------------------------------------------------------------------------------------------|");
-
+        System.out.println("|---------------------------------------------------------------------------------------------------------------------------------------|");
 
     }
     public static String[] studentsNames(){
@@ -48,13 +48,18 @@ public class StudentsMarksSystem {
         String[] studentNames = studentsNames();
         String[] studentSubjects = studentSubjects();
         ArrayList<String> studentsInformation = new ArrayList<>();
+        ArrayList<String> studentsIds = new ArrayList<>();
+        int studentIDCounter = 1;
 
         for (String studentName: studentNames){
             int studentTotalMarks = 0;
-            double studentAverageMarks = 0;
+            int studentID =studentIDCounter++ ;
+            double studentAverageMarks;
             System.out.println("Enter marks for "+studentName);
             ArrayList<String> studentSubjectsMarks = new ArrayList<>();
-
+            studentsIds.add(String.valueOf(studentID));
+            studentSubjectsMarks.add("|       "+studentID);
+            studentSubjectsMarks.add("|   "+studentName+"   ");
             for (String studentSubject: studentSubjects){
                 System.out.print("Enter marks for "+studentName+" for subject "+studentSubject+" :");
                 int marks = scanner.nextInt();
@@ -63,12 +68,36 @@ public class StudentsMarksSystem {
 
             }
             studentAverageMarks = (double) studentTotalMarks / studentSubjects.length;
-            studentSubjectsMarks.add("||                   "+studentTotalMarks+"   ||");
-            studentSubjectsMarks.add("           "+studentAverageMarks+"      ||");
+            String studentPerformance = "";
+            if(studentAverageMarks >= 70) {
+                studentPerformance ="Excellent";
+            }
+            else if (studentAverageMarks >= 60){
+                studentPerformance ="Very Good";
+
+            }
+            else if (studentAverageMarks >= 50){
+                studentPerformance ="Average  ";
+
+            }
+            else if (studentAverageMarks >= 40){
+                studentPerformance ="Pass     ";
+
+            }
+            else {
+                studentPerformance ="Fail     ";
+
+            }
+            studentSubjectsMarks.add("||        "+studentTotalMarks+"       ||");
+            studentSubjectsMarks.add("       "+studentAverageMarks+"    ||");
+            studentSubjectsMarks.add(studentPerformance+"       |");
 
             studentsInformation.add(""+studentSubjectsMarks);
         }
 
         return studentsInformation;
+    }
+    public static void editStudentMarks(){
+
     }
 }
