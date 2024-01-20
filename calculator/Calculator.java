13a162d5bf40b3d5f25897f690e3f305aa7b9085
nzangi <nzangimuoki284@gmail.com>
2024-01-20 13:59:48 +0300

    done working with simple console calculator

--- calculator/Calculator.java
+++ calculator/Calculator.java
@@ -4,9 +4,44 @@ import java.util.Scanner;
 
 public class Calculator {
     public static void main(String [] args){
-        Scanner scanner = new Scanner(System.in);
-        int numberOne,numberTwo;
+        doCalculation();
+    }
+
+//    The method does calculation after getting number and number two
+    public static void doCalculation(){
+        int numberOne = getInputNumberOne();
+        int numberTwo = getInputNumberTwo();
         String operand="";
+        Scanner scanner = new Scanner(System.in);
+        while (true){
+            try {
+                System.out.print("Enter the operation do do with the two numbers: ");
+                operand = scanner.next();
+                String[] operands = {"/", "*", "%", "+", "-"};
+                for (String stringOperand : operands) {
+                    if (operand.equals(stringOperand)) {
+                        System.out.println("Your operand is:" + operand);
+//                        the switchPrint method arguments are given here
+                        switchPrint(numberOne,numberTwo,operand);
+                        System.out.println();
+                        System.out.println("The Project is Over!");
+                        return;
+                    }
+                }
+                System.out.println("Enter a valid operand: ");
+                scanner.nextLine();
+            }
+            catch (Exception e) {
+                System.out.println("An error occurred. Please try again.");
+                scanner.nextLine();
+            }
+        }
+
+    }
+//    The method gets the first number
+    public static int getInputNumberOne(){
+        int numberOne;
+        Scanner scanner = new Scanner(System.in);
         while (true){
             try {
                 System.out.print("Enter the value of number One: ");
@@ -17,7 +52,14 @@ public class Calculator {
                 scanner.nextLine();
             }
         }
+        return  numberOne;
+    }
 
+    //    The method gets the second number
+
+    public static int getInputNumberTwo(){
+        int numberTwo;
+        Scanner scanner = new Scanner(System.in);
 
         while (true){
             try {
@@ -29,32 +71,12 @@ public class Calculator {
                 scanner.nextLine();
             }
         }
-
-        while (true){
-                try {
-                    System.out.print("Enter the operation do do with the two numbers: ");
-                    operand = scanner.next();
-                    String[] operands = {"/", "*", "%", "+", "-"};
-                    for (String stringOperand : operands) {
-                        if (operand.equals(stringOperand)) {
-                            System.out.println("Your operand is:" + operand);
-                        }
-                    }
-                    break;
-                }
-                catch (Exception e) {
-                    System.out.print("Enter a valid operand: ");
-                    scanner.nextLine();
-                }
-        }
-        switchPrint(numberOne,numberTwo,operand);
-
-
-
-//        scanner.close();
-
+        return numberTwo;
 
     }
+
+    //    The method is work is to do the actual calculation by calling the parameters function such as addition etc
+
     public static void switchPrint(int numberOne,int numberTwo,String operand){
         switch (operand){
             case "+":
@@ -72,28 +94,34 @@ public class Calculator {
             case "*":
                 System.out.print("The multiplication of "+numberOne+" and "+ numberTwo+" is : "+multiplication(numberOne,numberTwo));
                 break;
-//            default:
-//                System.out.println("No such Operand. Try again");
-
         }
     }
-
+//The sum methods, it takes two parameters
     public static int addition(int numberOne,int numberTwo){
         int sum=numberOne +numberTwo;
         return sum;
     }
+    //The multiplication method, it takes two parameters
+
     public static int multiplication(int numberOne,int numberTwo){
         int product=numberOne * numberTwo;
         return product;
     }
+    //The subtraction method, it takes two parameters
+
     public static int subtraction(int numberOne,int numberTwo){
         int subtraction=numberOne - numberTwo;
         return subtraction;
     }
+
+    //The division method, it takes two parameters
+
     public static int division(int numberOne,int numberTwo){
         int division=numberOne / numberTwo;
         return division;
     }
+    //The modules method, it takes two parameters
+
     public static int modulas(int numberOne,int numberTwo){
         int modula=numberOne % numberTwo;
         return modula;
